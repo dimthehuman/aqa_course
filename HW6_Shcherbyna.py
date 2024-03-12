3451eb78b0403ff0f8cdc1bd719232e99fc06f03
Dmytro Shcherbyna <d4nteua@outlook.com>
2024-03-12 15:52:49 +0200

    Updated HW6 commit

--- HW6_Shcherbyna.py
+++ HW6_Shcherbyna.py
@@ -1,6 +1,6 @@
 # Task 1 - call sum_range() function for testing
 # Task 2 - call square() function for testing
-# Task 3 - call data_type_checker() function for testing
+# Task 3 - call process_user_input() function and pass get_user_input() as parameter for testing
 
 def sum_range(start, end):
     if start > end:
@@ -16,28 +16,27 @@ def square(side):
     print(f"Perimeter: {perimeter}. Area: {area}. Diagonal: {diagonal}.")
 
 
-def data_type_checker():
-    def get_user_input():
-        user_input = input("Please enter the data: ")
-        return user_input
+def get_user_input():
+    user_input = input("Please enter the data: ")
+    return user_input
 
-    def process_user_input(data):
-        data_type = TypeError("Unknown data type")
-        try:
-            if isinstance(eval(data), dict):
-                data_type = dict
-            elif isinstance(eval(data), list):
-                data_type = list
-            elif isinstance(eval(data), int):
-                data_type = int
-            elif isinstance(eval(data), tuple):
-                data_type = tuple
-        except NameError:
-            print("Unknown data type.")
-        else:
-            print(f"User is going to work with {data_type}")
 
-    data_to_check = get_user_input()
+def process_user_input(data):
+    data_type = None
+    try:
+        if isinstance(eval(data), dict):
+            data_type = dict
+        elif isinstance(eval(data), list):
+            data_type = list
+        elif isinstance(eval(data), int):
+            data_type = int
+        elif isinstance(eval(data), tuple):
+            data_type = tuple
+    except NameError:
+        print("Unknown data type.")
+    else:
+        print(f"User is going to work with {data_type}")
 
-    process_user_input(data_to_check)
+
+
 
